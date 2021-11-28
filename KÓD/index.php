e453c6b05e95d393da52d9026cc47526ed6d8b17
BocekOndrej <bocek04@student.vspj.cz>
2021-11-28 13:39:44 +0100

    aktualizace footeru

--- KÓD/index.php
+++ KÓD/index.php
@@ -2,6 +2,14 @@
   require "header.php";
   require "connectDB.php";
  ?>
+ <div id="ind">
+  <p class="text-center lead">Vítejte na stránkách časopisu</p>
+  <p class="text-center display-1">Zelené levo</p>
+</div>
+
+
+
+
     </div>
   </div>
 </div>
