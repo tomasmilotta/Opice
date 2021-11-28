65f3c9be1df58ba7e6437b0f8d69646e796a0059
BocekOndrej <bocek04@student.vspj.cz>
2021-11-28 13:45:13 +0100

    aktualizace footeru

--- KÓD/footer.php
+++ KÓD/footer.php
@@ -2,9 +2,10 @@
  mysqli_close($spojeni);
  ?>
 
- <footer id="footer">
-  <a class="navbar-brand" href="index.php"><img src="img/logo2.png" alt="" height="58"></a>
-
+<div class="push"></div>
+</div>
+<footer class="footer">
+  <a class="d-flex justify-content-center" href="index.php"><img src="img/logo2.png" alt="" height="40"></a>
   <div id="madeby">Made by Opice 2021 &copy;</div>
 </footer>
   </body>
--- KÓD/header.php
+++ KÓD/header.php
@@ -13,12 +13,13 @@
         echo "Zelené Levo";
       }
     ?></title>
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/logo.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </head>
   <body>
+    <div class="wrapper">
   <nav class="navbar navbar-expand-lg navbar-light" id="navik">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php"><img src="img/logo2.png" alt="" height="58"></a>
