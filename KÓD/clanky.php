0438bcf57aafa31d3d8f91418080c6711d824b45
broz07 <92468812+broz07@users.noreply.github.com>
2022-01-08 12:42:06 +0100

    Delete KÓD directory

--- KÓD/clanky.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require "header.php";
-  require "connectDB.php";
- ?>
- <p class="text-center lead">Vydaná čísla: TODO</p>
-<?php
-  require "footer.php";
- ?>
--- KÓD/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();
-$_SESSION['msg-good']="Byl jste odhlášen";
-header("location:index.php");
-exit;
-?>
