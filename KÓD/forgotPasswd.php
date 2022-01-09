56572932b6dcce631513505e786a3694d3b6de9a
BocekOndrej <bocek04@student.vspj.cz>
2022-01-09 14:38:49 +0100

    Finalni verze designu

--- KÓD/forgotPasswd.php
+++ KÓD/forgotPasswd.php
@@ -2,17 +2,23 @@
   require "header.php";
   require "connectDB.php";
 ?>
-<h1>Zapomenuté heslo:</h1>
+<h1 align="center">Zapomenuté heslo</h1>
+
+<div class="d-flex justify-content-center mb-3">
+     <div class="d-inline-flex" id="reg">
 <form action="sendPass.php" method="post">
   <table>
     <tr>
-      <td>Váš email nebo login</td><td> <input type="text" name="input" required></td>
+      <td>Váš email nebo login</td><td> <input type="text" name="input" required class="form-control"></td>
     </tr>
     <tr>
-      <td colspan="2"> <input type="submit"> </td>
+      <td colspan="2" align="center"> <input type="submit" value="Odeslat" id="reg-but"></td>
     </tr>
   </table>
 </form>
+
+</div>
+</div>
 <?php
   require "footer.php";
 ?>
